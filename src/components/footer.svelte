0e3d3e64212ea425d54b1c42b8cdd0758d379858
wilbertJosh <114340373+wilbertJosh@users.noreply.github.com>
2025-07-27 17:07:03 +0800

    rev

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -6,6 +6,8 @@
   class="flex items-center justify-start max-w-screen-md lg:mx-auto md:mx-auto sm:mx-3 xs:mx-3 h-auto px-4 mb-3 py-4 space-y-3 mx-3"
 >
   <p class="flex items-center text-sm sm:text-base md:text-lg lg:text-xl">
-    <span class="ml-1">&nbsp;wbert © {currentYear}All Rights Reserved</span>
+    <span class="ml-1"
+      >&nbsp;wbert © {currentYear}&nbsp;All Rights Reserved</span
+    >
   </p>
 </div>
