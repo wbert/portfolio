6f43f913f896eafa15ed01ebaea94f8faabe4c07
wilbertJosh <114340373+wilbertJosh@users.noreply.github.com>
2025-07-27 17:01:26 +0800

    revisions on e-portfolio

--- src/components/Experience.svelte
+++ src/components/Experience.svelte
@@ -5,7 +5,7 @@
 <div
   class="flex flex-col items-start justify-between max-w-screen-md lg:mx-auto md:mx-auto sm:mx-3 xs:mx-3 h-auto px-4 border border-gray-300 rounded-lg mb-3 py-4 space-y-3 mx-3"
 >
-  <h3 class="text-xl font-semibold">💼 &nbsp Professional &nbsp Experience</h3>
+  <h3 class="text-xl font-semibold">💼&nbspProfessional&nbspExperience</h3>
   {#each props.experience as exp}
     <div class="space-y-2">
       <h4 class="text-lg font-semibold">{exp.role} at {exp.company}</h4>
